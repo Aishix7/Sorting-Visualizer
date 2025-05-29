456a66ae7a75c6b10d507828c88ca1102b2ef53d
Aishix7 <hithaishi714@gmail.com>
2025-05-29 18:16:15 +0530

    added new random reset button

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -5,6 +5,9 @@ function Buttons() {
     <div className="border bg-pink-50 p-3 border-black mt-6 w-fit shadow-xl mx-auto">
       <div className="flex gap-3 justify-center">
         <button className="border bg-purple-200 border-black p-2 font-medium text-black-700">
+          Random Reset
+        </button>
+        <button className="border bg-purple-200 border-black p-2 font-medium text-black-700">
           Skip To First
         </button>
         <button className="border bg-purple-200 border-black p-2 font-medium text-black-700">
